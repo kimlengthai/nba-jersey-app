adfa6145f2ef101e6353c35cef3e7dfe169df127
Kimleng <141606539+kimlengthai@users.noreply.github.com>
2025-06-17 10:50:15 +1000

    Implement hash password

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const UserModel = require('./models/User');
+const bcrypt = require('bcrypt');
 
 dotenv.config();
 
@@ -14,33 +15,45 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-app.post('/login', (req, res) => 
+app.post('/login', async (req, res) => 
     {
         const { email, password } = req.body;
-        UserModel.findOne({ email: email })
-            .then(user => {
-                if (user) 
-                {
-                    if (user.password === password) 
-                    {
-                        res.json({ status: "Success", user });
-                    } else 
-                    {
-                        res.json({ status: "Error", message: "The password is incorrect" });
-                    }
-                } 
-                else 
-                {
-                    res.json({ status: "Error", message: "User not found" });
-                }
-            })
+    
+        try
+        {
+            const user = await UserModel.findOne({ email });
+
+            if (!user) 
+            {
+                return res.json({ status: "Error", message: "User not found" });
+            }
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch)
+            {
+                return res.json({ status: "Error", message: "Incorect password" });
+            }
+
+            res.json({ status: "Success", user });
+        }
+        catch (err)
+        {
+            res.status(500).json({ status: "Error", message: "Server error", error: err.message });
+        }
     });
 
-app.post('/register', (req, res) => 
+app.post('/register', async (req, res) => 
     {
-        UserModel.create(req.body)
-            .then(user => res.json(user))
-            .catch(err => res.status(400).json(err));
+        try 
+        {
+            const { name, email, password } = req.body;
+            const hash = await bcrypt.hash(password, 10);
+            const user = await UserModel.create({ name, email, password: hash});
+            res.json(user);
+        } catch (err)
+        {
+            res.status(400).json(error);
+        }
     });
 
 app.listen(process.env.PORT, () => 
