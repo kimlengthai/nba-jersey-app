62043059d528a7b00d96c7a150ffa0c1a5bbd344
Kimleng <141606539+kimlengthai@users.noreply.github.com>
2025-06-22 03:12:32 +1000

    import image dynamically to avoid redundant image imports

--- client/src/pages/Catalogue.jsx
+++ client/src/pages/Catalogue.jsx
@@ -3,72 +3,9 @@ import axios from 'axios';
 import { Link, useLocation } from 'react-router-dom';
 import './Catalogue.css';
 import { useCart } from '../context/CartContext';
-import James from '../assets/James.png';
-import Curry from '../assets/Curry.png';
-import Ball from '../assets/Ball.png';
-import Banchero from '../assets/Banchero.png';
-import Barnes from '../assets/Barnes.png';
-import Bridges from '../assets/Bridges.png';
-import Brunson from '../assets/Brunson.png';
-import Butler from '../assets/Butler.png';
-import Cade from '../assets/Cade.png';
-import Durant from '../assets/Durant.png';
-import Edwards from '../assets/Edwards.png';
-import Embiid from '../assets/Embiid.png';
-import Fox from '../assets/Fox.png';
-import Giannis from '../assets/Giannis.png';
-import Green from '../assets/Green.png';
-import Hali from '../assets/Hali.png';
-import Ja from '../assets/Ja.png';
-import Jokic from '../assets/Jokic.png';
-import Kawhi from '../assets/Kawhi.png';
-import Kuzma from '../assets/Kuzma.png';
-import Lauri from '../assets/Lauri.png';
-import Lavine from '../assets/Lavine.png';
-import Luka from '../assets/Luka.png';
-import Mitchell from '../assets/Mitchell.png';
-import SGA from '../assets/SGA.png';
-import Simons from '../assets/Simons.png';
-import Tatum from '../assets/Tatum.png';
-import Wemby from '../assets/Wemby.png';
-import Young from '../assets/Young.png';
-import Zion from '../assets/Zion.png';
 import Placeholder from '../assets/placeholder.png';
 import { apiUrl } from '../utils/api';
-
-const imageMap = 
-{
-  "James.png": James,
-  "Curry.png": Curry,
-  "Ball.png": Ball,
-  "Banchero.png": Banchero,
-  "Barnes.png": Barnes,
-  "Bridges.png": Bridges,
-  "Brunson.png": Brunson,
-  "Butler.png": Butler,
-  "Cade.png": Cade,
-  "Durant.png": Durant,
-  "Edwards.png": Edwards,
-  "Embiid.png": Embiid,
-  "Fox.png": Fox,
-  "Giannis.png": Giannis,
-  "Green.png": Green,
-  "Hali.png": Hali,
-  "Ja.png": Ja,
-  "Jokic.png": Jokic,
-  "Kawhi.png": Kawhi,
-  "Kuzma.png": Kuzma,
-  "Lauri.png": Lauri,
-  "Lavine.png": Lavine,
-  "Luka.png": Luka,
-  "Mitchell.png": Mitchell,
-  "SGA.png": SGA,
-  "Simons.png": Simons,
-  "Tatum.png": Tatum,
-  "Wemby.png": Wemby,
-  "Young.png": Young,
-  "Zion.png": Zion
-}
+import { imageMap } from '../assets/playerImages';
 
 const easternTeams = 
 [
